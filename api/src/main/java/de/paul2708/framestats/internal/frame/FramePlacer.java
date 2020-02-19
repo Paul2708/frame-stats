2db60b0f2f9e7d1766b5620b2fc16b1b23ceea00
Paul2708 <paul.hoger@gmx.net>
2020-02-19 20:01:32 +0100

    Add and implement frame placer

---
+++ api/src/main/java/de/paul2708/framestats/internal/frame/FramePlacer.java
@@ -0,0 +1,136 @@
+package de.paul2708.framestats.internal.frame;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.ItemFrame;
+
+import java.util.Optional;
+
+/**
+ * This class handles searching and placing item frames and their map content.
+ *
+ * @author Paul2708
+ */
+public final class FramePlacer {
+
+    private static final short START_ID = 1000;
+
+    // TODO: Format comment
+
+    /**
+     * Search for frames.
+     *
+     * @param blocks searched array of blocks
+     * @return array of item frames
+     * @throws IllegalArgumentException if a block doesn't has an item frame
+     */
+    public ItemFrame[][] search(Block[][] blocks) {
+        ItemFrame[][] frames = new ItemFrame[blocks.length][blocks[0].length];
+
+        for (int i = 0; i < frames.length; i++) {
+            for (int j = 0; j < blocks[0].length; j++) {
+                Optional<ItemFrame> searched = searchSingle(blocks[i][j]);
+
+                if (searched.isPresent()) {
+                    frames[i][j] = searched.get();
+                } else {
+                    throw new IllegalArgumentException("block " + blocks[i][j].getLocation() + " has no item frame");
+                }
+            }
+        }
+
+        return frames;
+    }
+
+    /**
+     * Construct a 2-dimensional array of blocks.
+     * The following matrix will show the order:
+     * <br>
+     * <code>entry[0][0]</code> ... <code>rightCorner</code><br>
+     * .                                    .<br>
+     * .                                    .<br>
+     * .                                    .<br>
+     * <code>leftCorner</code> ...  <code>entry[m][n]</code>
+     *
+     * @param leftCorner left corner location
+     * @param rightCorner right corner location
+     * @return constructed array of blocks
+     */
+    public Block[][] construct(Location leftCorner, Location rightCorner) {
+        // TODO: Refactor, duplicated code
+        if (leftCorner.getBlockX() == rightCorner.getBlockX()) {
+            int widthBlocks = 1 + Math.abs(leftCorner.getBlockZ() - rightCorner.getBlockZ());
+            int heightBlocks = 1 + Math.abs(leftCorner.getBlockY() - rightCorner.getBlockY());
+
+            Block[][] blocks = new Block[widthBlocks][heightBlocks];
+
+            for (int i = 0; i < heightBlocks; i++) {
+                for (int j = 0; j < widthBlocks; j++) {
+                    int zCoordinate = leftCorner.getBlockZ();
+
+                    if (leftCorner.getBlockZ() < rightCorner.getBlockZ()) {
+                        zCoordinate += j;
+                    } else {
+                        zCoordinate -= j;
+                    }
+
+                    Location location = new Location(leftCorner.getWorld(), leftCorner.getBlockX(),
+                            rightCorner.getBlockY() - i, zCoordinate);
+
+                    blocks[j][i] = location.getBlock();
+                }
+            }
+
+            return blocks;
+        } else {
+            int widthBlocks = 1 + Math.abs(leftCorner.getBlockX() - rightCorner.getBlockX());
+            int heightBlocks = 1 + Math.abs(leftCorner.getBlockY() - rightCorner.getBlockY());
+
+            Block[][] blocks = new Block[widthBlocks][heightBlocks];
+
+            for (int i = 0; i < heightBlocks; i++) {
+                for (int j = 0; j < widthBlocks; j++) {
+                    int xCoordinate = leftCorner.getBlockX();
+
+                    if (leftCorner.getBlockX() < rightCorner.getBlockX()) {
+                        xCoordinate += j;
+                    } else {
+                        xCoordinate -= j;
+                    }
+
+                    Location location = new Location(leftCorner.getWorld(), xCoordinate,
+                            rightCorner.getBlockY() - i, leftCorner.getBlockZ());
+
+                    blocks[j][i] = location.getBlock();
+                }
+            }
+
+            return blocks;
+        }
+    }
+
+    /**
+     * Search an hanging item frame at a block.
+     * If multiple frames were fount, the first will be returned.
+     *
+     * @param block block to search for
+     * @return optional item frame
+     */
+    private Optional<ItemFrame> searchSingle(Block block) {
+        Location location = block.getLocation();
+
+        for (Entity entity : location.getWorld().getNearbyEntities(location, 2, 2, 2)) {
+            if (entity instanceof ItemFrame) {
+                ItemFrame frame = (ItemFrame) entity;
+                boolean correctSide =  entity.getLocation().getBlock().getRelative(frame.getAttachedFace()).equals(block);
+
+                if (correctSide) {
+                    return Optional.of((ItemFrame) entity);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+}
