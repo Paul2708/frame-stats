10e3cf31810eba96a47f12076a9fbf959853c8a0
Paul2708 <paul.hoger@gmx.net>
2020-02-19 17:50:57 +0100

    Load yaml configuration with first attributes

--- api/src/main/java/de/paul2708/framestats/configuration/TableConfiguration.java
+++ api/src/main/java/de/paul2708/framestats/configuration/TableConfiguration.java
@@ -1,6 +1,14 @@
 package de.paul2708.framestats.configuration;
 
 import de.paul2708.framestats.exception.InvalidConfigurationException;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 /**
  * This class holds all needed information about a table.
@@ -10,8 +18,35 @@ import de.paul2708.framestats.exception.InvalidConfigurationException;
  */
 public final class TableConfiguration {
 
-    private TableConfiguration() {
+    private static final String PATH = "./plugins/Frame-Stats/";
 
+    private final FileConfiguration configuration;
+
+    /**
+     * Load the yaml configuration.
+     *
+     * @param path an existing absolute path to the configuration
+     */
+    private TableConfiguration(String path) {
+        this.configuration = YamlConfiguration.loadConfiguration(new File(path));
+    }
+
+    /**
+     * Get the left lower corner of the frames wall.
+     *
+     * @return left lower corner
+     */
+    public Location getLeftLowerCorner() {
+        return (Location) configuration.get("locations.left-lower-corner");
+    }
+
+    /**
+     * Get the right upper corner of the frames wall.
+     *
+     * @return right upper corner
+     */
+    public Location getRightUpperCorner() {
+        return (Location) configuration.get("locations.right-upper-corner");
     }
 
     /**
@@ -23,7 +58,16 @@ public final class TableConfiguration {
      * see more in {@link InvalidConfigurationException#getMessage()}
      */
     public static TableConfiguration load(String path) throws InvalidConfigurationException {
-        // TODO: Implement me
-        return null;
+        String fullPath = TableConfiguration.PATH + path;
+        if (Files.notExists(Paths.get(fullPath))) {
+            throw new InvalidConfigurationException(String.format("Configuration file %s not found", fullPath));
+        }
+
+        TableConfiguration configuration = new TableConfiguration(fullPath);
+
+        Objects.requireNonNull(configuration.getLeftLowerCorner(), "Left lower corner must be set");
+        Objects.requireNonNull(configuration.getRightUpperCorner(), "Right upper corner must be set");
+
+        return configuration;
     }
 }
