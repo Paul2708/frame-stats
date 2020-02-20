cef3b7efd2a7e1faf471ab81f76fb71c7cd73b09
Paul2708 <paul.hoger@gmx.net>
2020-02-20 21:54:43 +0100

    Add raw table view implementation and table registration

---
+++ api/src/main/java/de/paul2708/framestats/internal/TableRegistration.java
@@ -0,0 +1,47 @@
+package de.paul2708.framestats.internal;
+
+import de.paul2708.framestats.table.Table;
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class description.
+ *
+ * @author Paul2708
+ */
+public final class TableRegistration {
+
+    private static TableRegistration instance;
+
+    private final Map<Table, MapView[][]> tables;
+    // private final Map<UUID, List<TableView>> views;
+
+    private TableRegistration() {
+        this.tables = new HashMap<>();
+    }
+
+    public void registerPlayer(Player player) {
+        tables.forEach((table, maps) -> {
+            TableView view = new TableView(table, maps, player);
+
+            table.addView(view);
+            view.setup();
+            view.draw();
+        });
+    }
+
+    public void registerTable(Table table, MapView[][] views) {
+        tables.put(table, views);
+    }
+
+    public static TableRegistration getInstance() {
+        if (TableRegistration.instance == null) {
+            TableRegistration.instance = new TableRegistration();
+        }
+
+        return TableRegistration.instance;
+    }
+}
--- api/src/main/java/de/paul2708/framestats/internal/TableView.java
+++ api/src/main/java/de/paul2708/framestats/internal/TableView.java
@@ -1,7 +1,25 @@
 package de.paul2708.framestats.internal;
 
+import de.paul2708.framestats.internal.frame.FramePlacer;
+import de.paul2708.framestats.internal.image.ImageSplitter;
 import de.paul2708.framestats.table.Table;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Class description.
@@ -12,13 +30,54 @@ public final class TableView {
 
     private final Table table;
     private final Player player;
+    private final MapView[][] view;
 
-    public TableView(Table table, Player player) {
+    private BufferedImage image;
+
+    public TableView(Table table, MapView[][] view, Player player) {
         this.table = table;
+        this.view = view;
         this.player = player;
     }
 
-    public void update() {
-        // TODO: Implement me
+    public void setup() {
+        try {
+            image = ImageIO.read(new File("./plugins/Download.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw() {
+        ImageSplitter splitter = new ImageSplitter();
+        Image[][] images = splitter.splitImage(image);
+
+        for (int i = 0; i < images.length; i++) {
+            for (int j = 0; j < images[0].length; j++) {
+                Image currentImage = images[i][j];
+                MapView view = this.view[i][j];
+
+                List<MapRenderer> list = new LinkedList<>(view.getRenderers());
+
+                for (MapRenderer renderer : list) {
+                    view.removeRenderer(renderer);
+                }
+
+                view.addRenderer(new MapRenderer() {
+
+                    boolean render = false;
+
+                    @Override
+                    public void render(MapView map, MapCanvas canvas, Player player) {
+                        if (!render) {
+                            canvas.drawImage(0, 0, currentImage);
+                            render = true;
+                        }
+                    }
+                });
+            }
+        }
+
+
     }
 }
