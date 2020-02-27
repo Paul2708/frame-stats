a8af62f7cc5d7600802bcf7fe8374b7aa6485971
Paul2708 <paul.hoger@gmx.net>
2020-02-27 23:19:13 +0100

    Add name receiver interface for searching objectives

---
+++ api/src/main/java/de/paul2708/framestats/internal/search/NameReceiver.java
@@ -0,0 +1,34 @@
+package de.paul2708.framestats.internal.search;
+
+import org.bukkit.entity.Player;
+
+import java.util.function.Consumer;
+
+/**
+ * This interface is used to receive the searching string from the player.
+ *
+ * @author Paul2708
+ */
+@FunctionalInterface
+public interface NameReceiver {
+
+    // TODO: Add sign or chat receiver
+
+    /**
+     * Receive the searched user by the user.
+     * The consumer accepts the searched string.
+     *
+     * @param player player that searches
+     * @param consumer consumer that accepts the search objective
+     */
+    void receive(Player player, Consumer<String> consumer);
+
+    /**
+     * Get a dummy receiver that accepts the player name itself every time.
+     *
+     * @return dummy name receiver
+     */
+    static NameReceiver dummyReceiver() {
+        return (player, consumer) -> consumer.accept(player.getName());
+    }
+}
